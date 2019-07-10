922eba7eb7513982a464d0496b654a7a78fd6e6f
owowww <kevinh60217@gmail.com>
2019-07-10 11:37:22 +0800

    Add files via upload

--- 104111118_20190710/Homework20190710_01.py
+++ 104111118_20190710/Homework20190710_01.py
@@ -11,10 +11,32 @@
 student = ["李柏叡", "張家豪", "黃庭孜", "闕伯翰", "呂湧儒", "黃凱裕", "王宏宇", "連于瑄", "黃彥霖", "田立廣"]
 print(student)#直接印出
 #print(student[::-1])#反轉[::-1]
+
+"--------逐一列印 左至右---------"
 for i in range(len(student)):
     print(student[i])#逐一列印 左至右
-
+    
+"--------逐一列印 右至左---------"
 for i in range(-1,-(len(student)+1),-1):
     print(student[i])#逐一列印 右至左  
     
-print(student[-1:-11:-1])
+"----------1到-10，間隔-1-------"  
+print(student[-1:-11:-1])#-1到-10，間隔-1
+
+"-----------刪除陣列元素---------"
+del student[3]#刪除陣列元素
+for i in range(len(student)):    
+    print(student[i])#逐一列印 左至右
+    
+print("---------重複n次-----------")
+student*=2#重複n次
+for i in range(len(student)):
+    print(student[i])
+    
+print("-----------刪除串列元素(remove)-------------")  
+n=student.count("闕伯翰")
+if n!=0:
+    for i in range(n):
+        student.remove("闕伯翰")
+for i in range(len(student)):
+    print(student[i])        
