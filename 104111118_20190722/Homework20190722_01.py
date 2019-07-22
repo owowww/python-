758dbaa4961bd53ab41ad605df327ac2045bd71e
owowww <kevinh60217@gmail.com>
2019-07-22 15:46:27 +0800

    Add files via upload

--- 104111118_20190722/Homework20190722_01.py
+++ 104111118_20190722/Homework20190722_01.py
@@ -7,9 +7,8 @@ import random
 with open("student.txt","r",encoding="UTF-8-SIG") as f:
     student=[]
     for line in f:
-        student.append(line.strip('\t\n'))       
-    
-    
+        student.append(line.strip('\t\n'))     
+list=[]   #輸出用的空字串
 Numberpeople = eval(input("輸入依人數分組:"))
 Numbergroups = eval(input("輸入依組數分組:"))
 studenta=student[1:11]
@@ -17,18 +16,26 @@ studenta=student[1:11]
 randomstudent=random.sample(studenta,k=10)#隨機
 
 print("依人數分組的結果")
+list.append("依人數分組的結果")
 for i in range(0,10,Numberpeople):#0到10間，間隔
     print(randomstudent[i:i+Numberpeople])
+    list.append(randomstudent[i:i+Numberpeople])
 
 print("依組數分組的結果")
+list.append("依組數分組的結果")
 
 if Numbergroups<=10:
         number=10//Numbergroups 
 else:
     print("錯誤")
+    list.append("錯誤")
     
 for k in range(0,10,number):
     print(randomstudent[k:k+number])
+    list.append(randomstudent[k:k+number])
     
-
-        
+with open("result.txt","w",encoding="UTF-8-SIG") as f:
+    for ii in list:
+        kk=' '.join([str(jj) for jj in ii])
+        f.write(kk+"\n")
+            
